b8663e6ef9a88b3852351501d3a1ce502e5237cf
John Zdanis <Zdanis.john@gmail.com>
2013-10-02 13:24:40 -0400

    Writing tests for pagination

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ end
 
 gem 'jquery-rails'
 
+gem 'kaminari'
+gem 'kaminari-bootstrap'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- spec/integration/pledge_flows_spec.rb
+++ spec/integration/pledge_flows_spec.rb
@@ -49,6 +49,36 @@ describe "Pledge Listing" do
 
       last_email.to.should == [@project.user.email]
       expect(last_email.to).to eq([@project.user.email])
+
+
+    end
+
+
+    it "should have pagination" do 
+      user = FactoryGirl.create :user
+      50.times { |i| FactoryGirl.create(:project, title: "Project #{i}", user: user) }
+
+      visit "/projects"
+
+      # Expect the most recently created projects on page 1 (8 PER PAGE)
+      page.should have_content('Project 49')
+      expect(page).to have_content('Project 49')
+
+      page.should have_no_content('Project 41')
+      expect(page).to have_no_content('Project 41')
+
+      page.should have_selector('li.project', count: 8)
+      expect(page).to have_selector('li.project', count: 8)
+
+      # Expect pagination link and click page 2
+      page.find('.pagination').click_link '2'
+
+      # Expect page 2 to have the next 8 projects
+      page.should have_content('Project 41')
+      expect(page).to have_content('Project 41')
+
+      page.should have_no_content('Project 32')
+      expect(page).to have_no_content('Project 32')
     end
   end
 end
