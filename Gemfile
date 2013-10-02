33824c6b730ec4ed6f170e31602848b78d304638
John Zdanis <Zdanis.john@gmail.com>
2013-10-02 18:27:25 -0400

    added mailer and styles

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '3.2.14'
 gem 'pg'
 gem 'sorcery'
 
+gem 'whenever', :require => false
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -7,4 +7,24 @@ class Pledge < ActiveRecord::Base
   validates  :user, :project_id, :user_id, presence: true
   validates_numericality_of :amount, greater_than: 0
 
+
+  def raised
+    self.pledges.sum(:amount)
+  end
+
+  def percent_raised
+    ((raised.to_f / goal) * 100).to_i
+  end
+
+  def succeeded?
+    percent_raised >= 100
+  end
+
+  def surpassed_goal?
+    percent_raised > 100
+  end
+
+  def number_of_supporters
+    self.supporters.uniq.count # if you make two pledges, you are still one supporter
+  end
 end
