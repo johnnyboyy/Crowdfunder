09f45eee009844c4acbaf32cd5c4da2f8431d282
John Zdanis <Zdanis.john@gmail.com>
2013-10-02 18:52:56 -0400

    fix mailer_spec and pass make all tests pass

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -8,23 +8,5 @@ class Pledge < ActiveRecord::Base
   validates_numericality_of :amount, greater_than: 0
 
 
-  def raised
-    self.pledges.sum(:amount)
-  end
 
-  def percent_raised
-    ((raised.to_f / goal) * 100).to_i
-  end
-
-  def succeeded?
-    percent_raised >= 100
-  end
-
-  def surpassed_goal?
-    percent_raised > 100
-  end
-
-  def number_of_supporters
-    self.supporters.uniq.count # if you make two pledges, you are still one supporter
-  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -12,4 +12,30 @@ class Project < ActiveRecord::Base
   validates :teaser, presence: true, length: {maximum: 255}
   validates :description, presence: true
   validates :goal, presence: true, numericality: {greater_than: 0, allow_blank: true}
+
+
+  def raised
+    self.pledges.sum(:amount)
+  end
+
+  def percent_raised
+    ((raised.to_f / goal) * 100).to_i
+  end
+
+  def succeeded?
+    percent_raised >= 100
+  end
+
+  def surpassed_goal?
+    percent_raised > 100
+  end
+
+  def number_of_supporters
+    self.supporters.uniq.count # if you make two pledges, you are still one supporter
+  end
+
+  def supporters
+    self.pledges.map(&:user).map(&:name)
+  end
+
 end
